e9b080ab83f47266d86eb679d0983ab2446233dd
adswqqe <xotjdbkdnj@naver.com>
2020-04-08 10:55:14 +0900

    프로젝트 업그레이드 전 세이브

---
+++ Assets/1.Script/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+        }
+    }
+}
